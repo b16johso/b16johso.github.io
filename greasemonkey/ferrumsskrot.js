343ca15ad0d74478eabbcc194437973fa1277c00
b16johso <b16johso@student.his.se>
2019-09-16 11:54:35 +0200

    Nästan färdigt

--- greasemonkey/ferrumsskrot.js
+++ greasemonkey/ferrumsskrot.js
@@ -1,9 +1,29 @@
-window.addEventListener('load', function() {
-    // your code here
-    alert("Nu kommer Figge Ferrum i egen hög person");
+// ==UserScript==
+// @name     Ferrums skrot Greasemonkey script
+// @namespace ferrumsskrot
+// @version  1
+// @grant    GM.openInTab
+// @include	 http://ferrumsskrot.gq:8888/administrator/index.php?option=com_content&view=article&layout=edit
+// @require  https://raw.githack.com/HGustavs/ContextFreeLib/master/js/contextfreegrammar.js
+// ==/UserScript==
 
-	document.getElementById("jform_title").value = "Kräftkalas, kräftkalas";
-  	
-  	var textEditor = document.getElementById("jform_articletext_ifr").contentWindow.document.getElementById("tinymce");
-  	textEditor.getElementByTagName("P")[0].innerHTML = "Kräftkalas, kräftkalas, nu är det kräftkalas!";
+console.log("The script is loaded");
+
+window.addEventListener('load', function() {
+  //alert("Nu kommer Figge Ferrum i egen hög person");
+  
+  // The randomized sentence is stored in the snus variable
+  var randomTitle = generate_sentence(0.5,0.5,0.5,0.5,null,null,null,null,null,null,null);
+  
+  var randomText = "";
+  for(var i = 0; i < 20; i++){
+    sentence = generate_sentence(0.5,0.5,0.5,0.5,null,null,null,null,null,null,null);
+    randomText += sentence;
+  }
+  
+  document.getElementById("jform_title").value = randomTitle;
+  unsafeWindow.tinyMCE.editors[0].insertContent(randomText);
+  
+  document.getElementById("jform_featured0").value = "1";
+  unsafeWindow.Joomla.submitbutton("article.save2new");
 }, false);
